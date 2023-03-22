6a7314925ba6098f1f5b52f99edf6aaf8a58ad61
LukeAnger <anger.luke@gmail.com>
2023-03-21 20:56:57 -0400

    added rotation animation

---
+++ index.js
@@ -0,0 +1,39 @@
+
+let cube = document.querySelector('.cube');
+
+let cubeRotation = 0;
+let hover = false;
+let speed = 0.005;
+
+setInterval(() => {
+  speed = speed > 0 ? -(Math.random() * 0.005) : Math.random() * 0.005;
+  console.log(speed)
+}, 10000);
+
+
+function render() {
+  if (hover) {
+    cubeRotation = 0;
+    return;
+  }
+  cubeRotation = cubeRotation + speed;
+
+
+
+  cube.style.transform = 'translate(100%, 50%) rotateY(' + cubeRotation + 'rad) rotateX(' + cubeRotation*0.67 + 'rad) rotateZ(' + cubeRotation*0.33 + 'rad)';
+  requestAnimationFrame(render);
+}
+
+hover ? null : requestAnimationFrame(render);
+
+// I want to pause the animation when the mouse hovers over the cube
+cube.addEventListener('mouseover', () => {
+  cube.style.transform = 'translate(100%, 50%)';
+  hover = true;
+});
+
+// I want to resume the animation when the mouse leaves the cube
+cube.addEventListener('mouseout', () => {
+  hover = false;
+  requestAnimationFrame(render);
+});
